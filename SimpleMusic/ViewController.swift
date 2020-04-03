fc9ada26458a8f573d62c38bd15dc692dd96ac8c
pilgwon <rlavlfrnjs12@gmail.com>
2020-04-04 00:49:38 +0900

    Initial Commit

---
+++ SimpleMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMusic
+//
+//  Created by PilGwonKim on 2020/04/04.
+//  Copyright © 2020 OvertheOceanApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
